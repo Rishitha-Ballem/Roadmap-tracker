1acef436c215a5d7c8e9d1a549c9733d0c23ffd5
Rishitha-Ballem <rishithaballem@gmail.com>
2025-11-09 16:09:39 +0530

    Updated output.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,7 +6,4 @@ output "public_ip" {
   value = aws_instance.roadmap_app.public_ip
 }
 
-output "web_url" {
-  description = "URL to access the application"
-  value       = "http://${aws_instance.roadmap_app.public_ip}"
-}
+
